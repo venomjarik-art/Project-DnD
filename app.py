eb319f097d6d0fa0ae2e8379bc51b7a80c593984
yaroslav <venomjarik@gmail.com>
2026-01-14 12:45:11 +0700

    библиотеки

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-print("hi")
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import random
+import json
+import os
