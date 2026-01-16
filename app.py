cd9b46994c5d3029edfd63aaadfc2a0ab1ea9e01
yaroslav <venomjarik@gmail.com>
2026-01-16 13:38:30 +0700

    запись карт в формате JSON

--- app.py
+++ app.py
@@ -19,3 +19,6 @@ def load_cards():
     with open(CARDS_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
     
+    def save_cards(cards):
+         with open(CARDS_FILE, "w", encoding="utf-8") as f:
+          json.dump(cards, f, ensure_ascii=False, indent=2)
